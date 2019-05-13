4f943e99b6e4e671bf900028cff0cff3a4560442
zl_lukai <17764530522@139.com>
2019-05-13 15:31:43 +0800

    pom修改

--- ManagerModule/src/main/java/com/kt/managermode/controller/system/UserManagerController.java
+++ ManagerModule/src/main/java/com/kt/managermode/controller/system/UserManagerController.java
@@ -1,5 +1,7 @@
 package com.kt.managermode.controller.system;
 
+import com.kt.managermode.bean.system.ServerResponse;
+import com.kt.managermode.util.ResultCodes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.kt.managermode.bean.ResultUtilBean;
 import com.kt.managermode.bean.system.UserBean;
 import com.kt.managermode.service.system.IUserManagerService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/userManagerController")
@@ -23,7 +28,7 @@ public class UserManagerController
     {
         return "system/userManager";
     }
-    
+
     @ResponseBody
     @RequestMapping(value = "/listUser", method = RequestMethod.POST)
     public ResultUtilBean listUser(Model model, UserBean userBean)
@@ -33,5 +38,19 @@ public class UserManagerController
             userBean.getRows(),
             userManagerService.listUser(userBean));
         return result;
+    }
+    
+    @ResponseBody
+    @RequestMapping(value = "/listUserVue", method = RequestMethod.POST)
+    public ServerResponse listUserVue(Model model, UserBean userBean)
+    {
+        ResultUtilBean result = ResultUtilBean.getResult(userBean.getPage(),
+            userManagerService.getCount(userBean),
+            userBean.getRows(),
+            userManagerService.listUser(userBean));
+        Map<String,Object> resultData = new HashMap<String,Object>();
+        resultData.put("data",result);
+        ServerResponse serverResponse = ServerResponse.createDefaultResult(ResultCodes.SUCCESS,"success",resultData);
+        return serverResponse;
     }
 }
