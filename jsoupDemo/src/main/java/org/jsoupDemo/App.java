d248fa417db01bdf71dd9758223e4fe00798f2dc
zl_lukai <17764530522@139.com>
2019-06-21 17:12:59 +0800

    添加飘天中文的爬取

--- jsoupDemo/src/main/java/org/jsoupDemo/App.java
+++ jsoupDemo/src/main/java/org/jsoupDemo/App.java
@@ -23,15 +23,24 @@ public class App
     static
     {
         //都市仙尊
-        URL_MAP.put("bxwx",new BxwxParse("https://www.bxwx.la/b/274/274556/","都市仙尊"));
+        //URL_MAP.put("bxwx",new BxwxParse("https://www.bxwx.la/b/274/274556/","都市仙尊"));
         //斗魔传承
-        URL_MAP.put("piaotianzw",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/76549/index.html","斗魔传承"));
+        //URL_MAP.put("piaotianzw",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/76549/index.html","斗魔传承"));
+        //URL_MAP.put("最强医圣",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/2285/index.html","最强医圣"));
+        //URL_MAP.put("都市最强仙尊",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/75132/index.html","都市最强仙尊"));
+        //URL_MAP.put("都市逍遥仙尊",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/56966/index.html","都市逍遥仙尊"));
+        //URL_MAP.put("超级鉴宝师",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/5685/index.html","超级鉴宝师"));
+        //URL_MAP.put("麻衣相士",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/75996/index.html","麻衣相士"));
+        //URL_MAP.put("都市仙尊",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/62572/index.html","都市仙尊"));
     }
     
     public static void main(String[] args) throws Exception
     {
-        ParseHtml bp = URL_MAP.get("piaotianzw");
-        bp.parseHtml();
+        for (Map.Entry<String,ParseHtml> entry : URL_MAP.entrySet())
+        {
+            ParseHtml bp = entry.getValue();
+            bp.parseHtml();
+        }
         
     }
        
