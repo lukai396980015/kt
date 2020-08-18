73c6f175911e189e3271bd3b55406a6b7fcf869d
zl_lukai <17764530522@139.com>
2020-08-18 17:47:45 +0800

    图书爬取

--- jsoupDemo/src/main/java/org/jsoupDemo/App.java
+++ jsoupDemo/src/main/java/org/jsoupDemo/App.java
@@ -1,16 +1,7 @@
 package org.jsoupDemo;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.nodes.TextNode;
-import org.jsoup.select.Elements;
 
 /**
  * Hello world!
@@ -31,7 +22,7 @@ public class App
         //URL_MAP.put("都市逍遥仙尊",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/56966/index.html","都市逍遥仙尊"));
         //URL_MAP.put("超级鉴宝师",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/5685/index.html","超级鉴宝师"));
         //URL_MAP.put("麻衣相士",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/75996/index.html","麻衣相士"));
-        URL_MAP.put("都市仙尊",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/62572/index.html","都市仙尊"));
+        //URL_MAP.put("都市仙尊",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/62572/index.html","都市仙尊"));
         //URL_MAP.put("疯狂炼妖系统",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/72418/index.html","疯狂炼妖系统"));
         //URL_MAP.put("真龙",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/72433/index.html","真龙"));
         //URL_MAP.put("北斗帝尊",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/51253/index.html","北斗帝尊"));
@@ -42,6 +33,15 @@ public class App
         //URL_MAP.put("我的老婆是狐狸",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/77083/index.html","我的老婆是狐狸"));
         //URL_MAP.put("超级吞噬系统",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/36278/index.html","超级吞噬系统"));
         //URL_MAP.put("陆压道君异界游",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/60214/index.html","陆压道君异界游"));
+        //URL_MAP.put("异世厨神",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/53718/index.html","异世厨神"));
+        //URL_MAP.put("绝世符神",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/41665/index.html","绝世符神"));
+        //URL_MAP.put("逆仙",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/70183/index.html","逆仙"));
+        //URL_MAP.put("史上最强炼气期",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/75990/index.html","史上最强炼气期"));
+        //URL_MAP.put("召唤千军",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/38525/index.html","召唤千军"));
+        //URL_MAP.put("异世灵武天下",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/37085/index.html","异世灵武天下"));
+        //URL_MAP.put("最强修真弃少",new PiaotianzwParseImpl("https://www.piaotianzw.com/book/77368/index.html","最强修真弃少"));
+        URL_MAP.put("剑神纵横异界",new PiaotianzwParseImpl("https://www.piaotianzw.com/book_38947/","剑神纵横异界"));
+        //URL_MAP.put("全能装逼系统",new Yd97ParseImpl("https://www.97yd.com/menu/651.html","全能装逼系统"));
     }
     
     public static void main(String[] args) throws Exception
